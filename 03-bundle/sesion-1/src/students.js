765c583c438dc3005989ab62ea184858e267da9f
acastells <36441239+acastells@users.noreply.github.com>
2023-10-31 11:53:52 +0100

    first sessions of bundling with webpack

---
+++ 03-bundle/sesion-1/src/students.js
@@ -0,0 +1,10 @@
+// ES6
+const userName = "Arnau";
+const greetings = `Hello ${userName}`;
+
+const averageScore = 90;
+const messageToDisplay = `The average score is ${averageScore};`
+
+document.write(messageToDisplay)
+
+console.log(greetings);
