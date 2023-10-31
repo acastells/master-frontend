bd037bb8eb6d49d89467532ef1ccdafda4f9d0bf
acastells <36441239+acastells@users.noreply.github.com>
2023-10-31 18:29:46 +0100

    first version entregable 05

--- 02-languages/src/02-entregables/ejercicio-05.ts
+++ 02-languages/src/02-entregables/ejercicio-05.ts
@@ -1,1 +1,31 @@
 console.log("************** DELIVERABLE 05 *********************");
+
+
+class SlothMachine {
+	coins: number;
+
+	constructor() {
+		this.coins = 0;
+	}
+
+	play() {
+		this.coins++;
+		if (getRandomBoolean() && getRandomBoolean() && getRandomBoolean()) {
+			console.log(`Congratulations!!!. You won ${this.coins.toString()} coins!!`)
+			this.coins = 0
+		} else {
+			console.log("Good luck next time!!")
+		}
+	}
+}
+
+function getRandomBoolean() {
+	return Math.random() < 0.5; // 50% true, 50% false
+}
+
+const machine1 = new SlothMachine();
+machine1.play();
+machine1.play();
+machine1.play();
+machine1.play();
+machine1.play();
