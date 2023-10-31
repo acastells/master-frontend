93a4f22c9c4af861e0fc8e3095205b597c8ef8eb
acastells <36441239+acastells@users.noreply.github.com>
2023-10-31 17:45:19 +0100

    first version 01 entregable

--- 02-languages/src/02-entregables/ejercicio-01.ts
+++ 02-languages/src/02-entregables/ejercicio-01.ts
@@ -1,1 +1,29 @@
 console.log("************** DELIVERABLE 01 *********************");
+
+// HEAD
+const head = (arr) => {
+	let [first_element] = arr
+	return first_element
+}
+
+// TAIL
+const tail = (arr) => {
+	let [first_element, ...rest] = arr
+	return rest
+}
+
+// INIT
+const init = (arr) => {
+	return arr.slice(0, arr.length - 1);
+};
+
+// LAST
+const last = (arr) => {
+	return arr[arr.length - 1]
+};
+
+let array = ["a", "b", "c", "d"]
+console.log(head(array))
+console.log(tail(array))
+console.log(init(array))
+console.log(last(array))
