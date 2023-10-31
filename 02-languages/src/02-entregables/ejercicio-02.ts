0adc032843b9f7009fed724da93b055f308ed63c
acastells <36441239+acastells@users.noreply.github.com>
2023-10-31 17:45:28 +0100

    first version 02 entregable

--- 02-languages/src/02-entregables/ejercicio-02.ts
+++ 02-languages/src/02-entregables/ejercicio-02.ts
@@ -1,1 +1,21 @@
 console.log("************** DELIVERABLE 02 *********************");
+
+
+const concat = (a, b) => {
+	return [...a, ...b]
+};
+
+const concatAll = (...arrs) => {
+	let resultArr = [];
+	for (let arr of arrs) {
+		resultArr = [...resultArr, ...arr]
+	}
+	return resultArr;
+}
+
+const arr1 = ["please"]
+const arr2 = ["concatenate", "me"]
+const arr3 = ["can", "you"]
+
+console.log(concat(arr1, arr2))
+console.log(concatAll(arr1, arr3, arr2))
