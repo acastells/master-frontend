3eae159187fb632e7df45b59a4637dff13eb3749
acastells <36441239+acastells@users.noreply.github.com>
2023-11-06 13:20:31 +0100

    configuring performance bundling

--- 03-bundle/sesion-1/webpack.prod.js
+++ 03-bundle/sesion-1/webpack.prod.js
@@ -1,13 +1,13 @@
 import { merge } from "webpack-merge";
-import common from "./webpack.common.js";
 import Dotenv from "dotenv-webpack";
 import MiniCssExtractPlugin from "mini-css-extract-plugin";
+import common from "./webpack.common.js";
 
 export default merge(common, {
   mode: "production",
   output: {
     filename: "js/[name].[chunkhash].js",
-    assetModuleFilename: "assets/[hash][ext][query]"
+    assetModuleFilename: "assets/[hash][ext][query]",
   },
   module: {
     rules: [
@@ -29,13 +29,33 @@ export default merge(common, {
       },
     ],
   },
+  optimization: {
+    runtimeChunk: "single",
+    splitChunks: {
+      cacheGroups: {
+        vendor: {
+          chunks: "all",
+          name: (module) => {
+            const packageName = module.context.match(
+              /[\\/]node_modules[\\/](.*?)([\\/]|$)/
+            )?.[1];
+            return packageName
+              ? `vendor/${packageName.replace("@", "")}`
+              : null;
+          },
+          test: /[\\/]node_modules[\\/]/,
+          enforce: true,
+        },
+      },
+    },
+  },
   plugins: [
     new MiniCssExtractPlugin({
       filename: "css/[name].[chunkhash].css",
       chunkFilename: "[id].css",
     }),
     new Dotenv({
-      path: "./prod.env"
-    })
+      path: "./prod.env",
+    }),
   ],
 });
