aa17a9a2860d0fcc0808406b4c8bec203f400136
acastells <36441239+acastells@users.noreply.github.com>
2023-11-10 22:01:32 +0100

    added basic react

---
+++ 03-bundle/sesion-vite/src/index.tsx
@@ -0,0 +1,8 @@
+import { createRoot } from "react-dom/client"
+import { HelloComponent } from "./hello"
+
+const root = createRoot(document.getElementById("root"))
+
+root.render(
+	<HelloComponent />
+)
--- 03-bundle/sesion-vite/vite.config.ts
+++ 03-bundle/sesion-vite/vite.config.ts
@@ -1,10 +1,12 @@
 import { defineConfig } from "vite";
 import checker from "vite-plugin-checker"
 import typescript from "@rollup/plugin-typescript";
+import react from "@vitejs/plugin-react";
 
 export default defineConfig({
 	plugins: [
-		checker({ typescript: true })
+		checker({ typescript: true }),
+		react()
 	],
 	build: {
 		sourcemap: true,
