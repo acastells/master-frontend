32f3f20d15a2320cc71adbf920a83dbfbd21711f
acastells <36441239+acastells@users.noreply.github.com>
2023-11-10 22:17:04 +0100

    adding environment vars

--- 03-bundle/sesion-vite/src/hello.tsx
+++ 03-bundle/sesion-vite/src/hello.tsx
@@ -1,5 +1,21 @@
-import { FC } from 'react';
+import { FC, useEffect, useState } from "react";
+import config from "./config";
 
 export const HelloComponent: FC = () => {
-	return <h2>Hello from react</h2>
-}
+	const [count, setCount] = useState(0);
+
+	useEffect(() => {
+		const id = setInterval(() => {
+			setCount((prevCount) => prevCount + 1);
+		}, 1_000);
+		return () => clearInterval(id);
+	}, []);
+
+	return <div>
+		<h2>Hello from React {count} </h2>
+		<span>aa</span>
+		<p>API_BASE: {config.API_BASE}</p>
+		<p>ENABLE_FEATURE_A: {config.ENABLE_FEATURE_A.toString()}</p>
+	</div>
+		;
+};
