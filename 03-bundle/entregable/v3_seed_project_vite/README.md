b987a662f08cc94dc889db5db256ad30c1d170bd
acastells <36441239+acastells@users.noreply.github.com>
2023-11-21 13:20:34 +0100

    installed and configuring vite-plugin-image-optimizer

--- 03-bundle/entregable/v3_seed_project_vite/README.md
+++ 03-bundle/entregable/v3_seed_project_vite/README.md
@@ -1,3 +1,9 @@
-npm create vite@latest // with react and typescript
+// https://vitejs.dev/guide/#scaffolding-your-first-vite-project
+// with react and typescript
+npm create vite@latest 
 
-npm install vite-plugin-image-optimizer --save-dev
+// install image and svg optimizer + config "vite.config.ts"
+// https://github.com/FatehAK/vite-plugin-image-optimizer
+npm install vite-plugin-image-optimizer --save-dev 
+npm install sharp --save-dev
+npm install svgo --save-dev
--- 03-bundle/entregable/v3_seed_project_vite/src/App.tsx
+++ 03-bundle/entregable/v3_seed_project_vite/src/App.tsx
@@ -1,10 +1,9 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import optimizeLogo from './assets/optimization-svgrepo-com.svg'
+import reactLogo from './assets/react.svg'
+import viteLogo from './assets/vite.svg'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
@@ -15,19 +14,14 @@ function App() {
         <a href="https://react.dev" target="_blank">
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
+        <a href="https://github.com/FatehAK/vite-plugin-image-optimizer" target="_blank">
+          <img src={optimizeLogo} className="logo logo-optimize" alt="logo optimize image" />
+        </a>
       </div>
-      <h1>Vite + React</h1>
+      <h1>Vite + React + <br></br> vite-plugin-image-optimizer</h1>
       <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
+        aa
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   )
 }
