71288d30af926d1d98a3930f1c091d2141808e1c
acastells <36441239+acastells@users.noreply.github.com>
2023-11-21 19:52:36 +0100

    added types

--- 02-languages/src/02-entregables/ejercicio-01.ts
+++ 02-languages/src/02-entregables/ejercicio-01.ts
@@ -1,29 +1,31 @@
 console.log("************** DELIVERABLE 01 *********************");
 
 // HEAD
-const head = (arr) => {
+const head = <T>(arr: T[]): T => {
 	let [first_element] = arr
 	return first_element
 }
 
 // TAIL
-const tail = (arr) => {
+const tail = <T>(arr: T[]): T[] => {
 	let [first_element, ...rest] = arr
 	return rest
 }
 
 // INIT
-const init = (arr) => {
+const init = <T>(arr: T[]): T[] => {
 	return arr.slice(0, arr.length - 1);
 };
 
 // LAST
-const last = (arr) => {
+const last = <T>(arr: T[]): T => {
 	return arr[arr.length - 1]
 };
 
-let array = ["a", "b", "c", "d"]
+let array: String[] = ["a", "b", "c", "d"]
+console.log(array) // --> el array al principio y al final debe ser el mismo por que las funciones implementadas son inmutables
 console.log(head(array))
 console.log(tail(array))
 console.log(init(array))
 console.log(last(array))
+console.log(array)
