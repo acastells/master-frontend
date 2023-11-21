462db4774350627d4aefe3bc5357fe06dc64153f
acastells <36441239+acastells@users.noreply.github.com>
2023-11-21 19:57:59 +0100

    added types + recursive method

--- 02-languages/src/02-entregables/ejercicio-02.ts
+++ 02-languages/src/02-entregables/ejercicio-02.ts
@@ -1,21 +1,29 @@
 console.log("************** DELIVERABLE 02 *********************");
 
-
-const concat = (a, b) => {
-	return [...a, ...b]
+const concat = <T>(a: T[], b: T[]): T[] => {
+  return [...a, ...b];
 };
 
-const concatAll = (...arrs) => {
-	let resultArr = [];
-	for (let arr of arrs) {
-		resultArr = [...resultArr, ...arr]
-	}
-	return resultArr;
-}
+const concatAll = <T>(...arrs: T[][]): T[] => {
+  let resultArr: T[] = [];
+  for (let arr of arrs) {
+    resultArr = [...resultArr, ...arr];
+  }
+  return resultArr;
+};
 
-const arr1 = ["please"]
-const arr2 = ["concatenate", "me"]
-const arr3 = ["can", "you"]
+const concatAllRecursive = <T>(...arrs: T[][]): T[] => {
+  if (arrs.length === 0) {
+    return [];
+  }
+  const [first, ...rest] = arrs;
+  return [...first, ...concatAllRecursive(...rest)];
+};
 
-console.log(concat(arr1, arr2))
-console.log(concatAll(arr1, arr3, arr2))
+const arr1 = ["please", "..."];
+const arr2 = ["concatenate", "me"];
+const arr3 = ["can", "you"];
+
+console.log(concat(arr1, arr2));
+console.log(concatAll(arr1, arr3, arr2));
+console.log(concatAllRecursive(arr1, arr3, arr2));
