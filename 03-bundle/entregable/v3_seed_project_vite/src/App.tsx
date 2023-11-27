01a4541fd9997143c8b783298af847c525c3b1f4
acastells <36441239+acastells@users.noreply.github.com>
2023-11-27 20:27:45 +0100

    renamed assets

--- 03-bundle/entregable/v3_seed_project_vite/src/App.tsx
+++ 03-bundle/entregable/v3_seed_project_vite/src/App.tsx
@@ -1,42 +1,69 @@
 import "./App.css";
-import optimizeLogo from "./assets/optimization-svgrepo-com.svg";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "./assets/viteLogo.svg";
-import image1 from "./assets/image1.jpg";
-import image2 from "./assets/image2.jpg";
+import optimizeLogo from "./assets/optimizationIcon.svg";
+import reactLogo from "./assets/reactIcon.svg";
+import viteLogo from "./assets/viteIcon.svg";
+import arrow from "./assets/arrow.png";
+import image1 from "./assets/image.jpg";
+import image2 from "./assets/image2.jpeg";
+import consoleSnapshot from "./assets/consoleSnapshot.png";
 
 function App() {
-  return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-        <a
-          href="https://github.com/FatehAK/vite-plugin-image-optimizer"
-          target="_blank"
-        >
-          <img
-            src={optimizeLogo}
-            className="logo logo-optimize"
-            alt="logo optimize image"
-          />
-        </a>
-      </div>
-      <h1>
-        Vite + React + <br></br> vite-plugin-image-optimizer
-      </h1>
+	return (
+		<>
+			<h1>Vite + React + vite-plugin-image-optimizer</h1>
 
-      <img src={image1}></img>
-      <img src={image2}></img>
-      <img src={viteLogo}></img>
-      <img src={reactLogo}></img>
-      <img src={optimizeLogo}></img>
-    </>
-  );
+			<div className="container">
+				<div className="container-col">
+					<a href="https://vitejs.dev" target="_blank">
+						<img src={viteLogo} className="logo" alt="Vite logo" />
+					</a>
+					<a href="https://react.dev" target="_blank">
+						<img
+							src={reactLogo}
+							className="logo react"
+							alt="logo react"
+						/>
+					</a>
+					<a
+						href="https://github.com/FatehAK/vite-plugin-image-optimizer"
+						target="_blank"
+					>
+						<img
+							src={optimizeLogo}
+							className="logo logo-optimize"
+							alt="logo optimize image"
+						/>
+					</a>
+					<a href="https://picsum.photos/" target="_blank">
+						<img src={image1} className="logo" alt="logo" />
+					</a>
+					<a href="https://picsum.photos/" target="_blank">
+						<img src={image2} className="logo" alt="logo" />
+					</a>
+					<a target="_blank">
+						<img src={arrow} className="logo" alt="logo" />
+					</a>
+				</div>
+
+				<div className="container-col">
+					<p>
+						<b>Vite</b> como herramienta de compilación
+					</p>
+					<p>
+						<b>React</b> como librería Javascript
+					</p>
+					<p>
+						<b>vite-plugin-image-optimizer</b> como plugin de
+						optimización de assets para producción. Intenta
+						optimizar todos los assets (svg, png, jpg,... ) cuando
+						se ejecuta "npm run build". Su configuración se puede
+						encontrar en vite.config.ts. Ejemplo en consola de la optimización:
+					</p>
+					<img src={consoleSnapshot} style={{width:750}}></img>
+				</div>
+			</div>
+		</>
+	);
 }
 
 export default App;
