a1a21da06f914ae52f34d2106d84a2fd545ec0e4
acastells <36441239+acastells@users.noreply.github.com>
2023-11-27 20:27:59 +0100

    setting assetsInlineLimit to 0

--- 03-bundle/entregable/v3_seed_project_vite/vite.config.ts
+++ 03-bundle/entregable/v3_seed_project_vite/vite.config.ts
@@ -3,16 +3,41 @@ import react from "@vitejs/plugin-react";
 import { ViteImageOptimizer } from "vite-plugin-image-optimizer";
 
 // https://vitejs.dev/config/
+// más config en: https://github.com/FatehAK/vite-plugin-image-optimizer
 
 export default defineConfig({
-  //assetsInclude: ['**/*.svg'],
-  //publicDir: "./src/assets/",
-  plugins: [
-    react(),
-    ViteImageOptimizer({
-      svg: {
-        floatPrecision: 0,
-      }
-    }),
-  ],
+	build: {
+		assetsInlineLimit: 0,
+	},
+	plugins: [
+		react(),
+		ViteImageOptimizer({
+			svg: {
+				floatPrecision: 0,
+			},
+			jpg: {
+				quality: 90,
+				progressive: true,
+				optimiseCoding: true,
+				mozjpeg: true,
+				trellisQuantisation: true,
+				overshootDeringing: true,
+				optimiseScans: true,
+			},
+			jpeg: {
+				quality: 90,
+				progressive: true,
+				optimiseCoding: true,
+				mozjpeg: true,
+				trellisQuantisation: true,
+				overshootDeringing: true,
+				optimiseScans: true,
+			},
+			png: {
+				compressionLevel: 9,
+				adaptiveFiltering: true,
+				palette: true,
+			},
+		}),
+	],
 });
