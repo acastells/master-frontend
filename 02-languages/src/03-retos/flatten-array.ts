e41cb4c2e20b8a39995c8fe1c4ec83ae7ce5770b
acastells <36441239+acastells@users.noreply.github.com>
2023-12-04 14:35:57 +0100

    TODO WIP type flatten array

--- 02-languages/src/03-retos/flatten-array.ts
+++ 02-languages/src/03-retos/flatten-array.ts
@@ -1,27 +1,29 @@
-type NestedArray<T> =
-	| T
-	| T[]
-	| Array<NestedArray<T>>
+// TODO WIP
 
 
-// WIP !!! Try to type it
+type NestedArray<T> = T | NestedArray<T>[]
 
-const sample: NestedArray<number> = [
-	1, 
-	[2, 3], 
-	[[4], [5, 6, [7, 8, [9]]]]];
 
-// using recursive technique
-const flattenArray = <T>(array: Array<NestedArray<T>>): T[] => {
-	return array.reduce<T[]>((flatArray, value, _, __) => {
+const sample: NestedArray<number> = [1, [2, 3], [[4], [5, 6, [7, 8, [9]]]]];
+const sample1: NestedArray<number> = [1];
+const sample2: NestedArray<number> = [[2, 3]];
+const sample3: NestedArray<number> = [[[4], [5, 6, [7, 8, [9]]]]];
+
+
+const flattenArray = <T>(array: NestedArray<T>): T[] => {
+
+	const reductorFunction = (flatArray: T[], value: NestedArray<T> | T, _, __) => {
 		if (Array.isArray(value)) {
 			return flatArray.concat(flattenArray(value));
 		} else {
-			return flatArray.concat(value as T); // casting
+			return flatArray.concat(value);
 		}
-	}, []); // valor inicial es un array vacio, que se va a ir modificando por la función reductora
+	};
+
+	return array.reduce(reductorFunction, [])
+	// valor inicial es un array vacio, que se va a ir modificando por la función reductora
 };
 
 const flattedSample = flattenArray(sample);
 console.log("ORIGINAL ->", sample);
-console.log("FLATTED ->", flattedSample); // should be [1, 2, 3, 4, 5, 6, 7, 8, 9];
+console.log("FLATTED ->", flattedSample); // será [1, 2, 3, 4, 5, 6, 7, 8, 9];
