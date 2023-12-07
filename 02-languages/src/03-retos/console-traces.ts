e9c70e6dd40dbb3971a92d4902826dc75d9cfce9
acastells <36441239+acastells@users.noreply.github.com>
2023-12-07 10:30:46 +0100

    refactoring

--- 02-languages/src/03-retos/console-traces.ts
+++ 02-languages/src/03-retos/console-traces.ts
@@ -1,46 +1,48 @@
 const delay = (ms) => new Promise((resolve) => setTimeout(resolve, ms));
 
 const showMessage = async ([time, message]) => {
-  await delay(time);
-  console.log(message);
+	await delay(time);
+	console.log(message);
 };
 
 const triggers = [
-  async () => await showMessage([600, "fourth"]), // ==== () => setTimeout(() => console.log("fourth"), 200),
-  async () => await showMessage([1000, "fifth"]),
-  async () => await showMessage([200, "second"]),
-  async () => await showMessage([400, "third"]),
+	async () => await showMessage([600, "fourth"]), // === () => setTimeout(() => console.log("fourth"), 600),
+	async () => await showMessage([1000, "fifth"]),
+	async () => await showMessage([200, "second"]),
+	async () => await showMessage([400, "third"]),
 ];
 
-const execPromisesSequential = (promises) => {
-	let currentPromise: Promise<void> = Promise.resolve();   //
-	promises.forEach((promise) => {	  
+const execAllPromisesSequentially = (promises) => {
+	let currentPromise: Promise<void> = Promise.resolve();
+	promises.forEach((promise) => {
 		currentPromise = currentPromise.then(() => promise()); // Chain currentPromise with nextPromise
 	});
 	return currentPromise;
-  };
+};
 
 const run = (triggers) => {
-  console.log("first");
+	console.log("first");
 
-  let index_triggered: number[] = []; // stores the indexes of the order that the promises have been resolved
-  let array_promises: Promise<void>[] = [];  // stores promises that are wrapping the original triggers
-  let new_promises_sorted: Promise<void>[] = [] // stores promises in the inverted order 
+	let promiseWrappers: Promise<void>[] = []; // stores promises that are wrapping the original triggers
+	let promisesSorted: Promise<void>[] = []; // stores promises in the order they should execute sequentially
 
-  triggers.forEach((trigger, index) => {
-	array_promises.push(new Promise((resolve) => trigger().then(() => { index_triggered.push(index); resolve(null)})))
-  });
+	triggers.forEach((trigger, index) => {
+		promiseWrappers.push(
+			new Promise((resolve) =>
+				trigger().then(() => {
+					promisesSorted.unshift(trigger); // push to the start, so the promises are already sorted
+					resolve(null);
+				})
+			)
+		);
+	});
 
-  Promise.all(array_promises).then(() => {
-	console.log("---")
-
-	while(index_triggered.length != 0){
-		new_promises_sorted.push(triggers[index_triggered.pop()])
-	}
-	execPromisesSequential(new_promises_sorted).then(() => {
-		console.log("first")
-	})
-  })
+	Promise.all(promiseWrappers).then(() => {
+		console.log("---");
+		execAllPromisesSequentially(promisesSorted).then(() => {
+			console.log("first");
+		});
+	});
 };
 
 run(triggers);
