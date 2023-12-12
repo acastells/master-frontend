56ce4465f3706660f8d449a54fe8d550dffca92f
acastells <36441239+acastells@users.noreply.github.com>
2023-12-12 20:31:56 +0100

    add dark theme

--- 04-frameworks/entregables/ex1/src/app.tsx
+++ 04-frameworks/entregables/ex1/src/app.tsx
@@ -2,19 +2,36 @@ import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import { FilterContextProvider } from "./contexts";
 import { DetailPage, ListPage, LoginPage } from "./pages";
+import { ThemeProvider, createTheme } from "@mui/material/styles";
+import CssBaseline from "@mui/material/CssBaseline";
 
 export const App = () => {
+	const darkTheme = createTheme({
+		palette: {
+			mode: "dark",
+		},
+	});
+
 	return (
-		<FilterContextProvider>
-			<Router>
-				<Routes>
-					<Route path="/" element={<LoginPage />} />
-					<Route path="/list" element={<ListPage />} />
-					<Route path="/detail/:id" element={<DetailPage />} />
-					<Route path="/rickandmorty/list" element={<DetailPage />} />
-					<Route path="/rickandmorty/detail/:id" element={<DetailPage />} />
-				</Routes>
-			</Router>
-		</FilterContextProvider>
+		<ThemeProvider theme={darkTheme}>
+			<CssBaseline />
+			<FilterContextProvider>
+				<Router>
+					<Routes>
+						<Route path="/" element={<LoginPage />} />
+						<Route path="/list" element={<ListPage />} />
+						<Route path="/detail/:id" element={<DetailPage />} />
+						<Route
+							path="/rickandmorty/list"
+							element={<DetailPage />}
+						/>
+						<Route
+							path="/rickandmorty/detail/:id"
+							element={<DetailPage />}
+						/>
+					</Routes>
+				</Router>
+			</FilterContextProvider>
+		</ThemeProvider>
 	);
 };
