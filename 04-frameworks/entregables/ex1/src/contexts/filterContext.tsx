d541d366ff274e7f5d1a8d45ac59456ce9ac8d49
acastells <36441239+acastells@users.noreply.github.com>
2023-12-12 17:41:42 +0100

    include perPage and page states

--- 04-frameworks/entregables/ex1/src/contexts/filterContext.tsx
+++ 04-frameworks/entregables/ex1/src/contexts/filterContext.tsx
@@ -1,8 +1,12 @@
 import React, { PropsWithChildren, createContext } from "react";
 
 interface FilterContextEntity {
-	filter: string;
-	setFilter: (filter: string) => void;
+	orgName: string;
+	perPage: number;
+	page: number;
+	setOrgName: (filter: string) => void;
+	setPerPage: (perPage: number) => void;
+	setPage: (page: number) => void;
 }
 
 export const FilterContext = createContext<FilterContextEntity>(null);
@@ -10,11 +14,13 @@ export const FilterContext = createContext<FilterContextEntity>(null);
 export const FilterContextProvider: React.FC<PropsWithChildren<{}>> = ({
 	children,
 }) => {
-	const [filter, setFilter] = React.useState<string>("lemoncode");
+	const [orgName, setOrgName] = React.useState<string>("lemoncode");
+	const [perPage, setPerPage] = React.useState<number>(5);
+	const [page, setPage] = React.useState<number>(1);
 
 	return (
 		<FilterContext.Provider
-			value={{ filter: filter, setFilter: setFilter }}
+			value={{ orgName, perPage, page, setOrgName, setPerPage, setPage }}
 		>
 			{children}
 		</FilterContext.Provider>
