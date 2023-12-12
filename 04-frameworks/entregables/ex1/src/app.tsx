763621d284cb832484365f70f59ef05a8cc55de8
acastells <36441239+acastells@users.noreply.github.com>
2023-12-12 20:34:12 +0100

    add dark theme

--- 04-frameworks/entregables/ex1/src/app.tsx
+++ 04-frameworks/entregables/ex1/src/app.tsx
@@ -21,14 +21,8 @@ export const App = () => {
 						<Route path="/" element={<LoginPage />} />
 						<Route path="/list" element={<ListPage />} />
 						<Route path="/detail/:id" element={<DetailPage />} />
-						<Route
-							path="/rickandmorty/list"
-							element={<DetailPage />}
-						/>
-						<Route
-							path="/rickandmorty/detail/:id"
-							element={<DetailPage />}
-						/>
+						<Route path="/rickandmorty/list" element={<DetailPage />} />
+						<Route path="/rickandmorty/detail/:id" element={<DetailPage />} />
 					</Routes>
 				</Router>
 			</FilterContextProvider>
