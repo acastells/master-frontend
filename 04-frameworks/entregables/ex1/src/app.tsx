64108fb462e207d323b61021987f7f7ef8e14313
acastells <36441239+acastells@users.noreply.github.com>
2023-12-12 17:19:28 +0100

    using FilterContextProvider

--- 04-frameworks/entregables/ex1/src/app.tsx
+++ 04-frameworks/entregables/ex1/src/app.tsx
@@ -3,15 +3,18 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import { LoginPage } from "./login";
 import { ListPage } from "./list";
 import { DetailPage } from "./detail";
+import { FilterContextProvider } from "./contexts";
 
 export const App = () => {
-  return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<LoginPage />} />
-        <Route path="/list" element={<ListPage />} />
-        <Route path="/detail/:id" element={<DetailPage />} />
-      </Routes>
-    </Router>
-  );
+	return (
+		<FilterContextProvider>
+			<Router>
+				<Routes>
+					<Route path="/" element={<LoginPage />} />
+					<Route path="/list" element={<ListPage />} />
+					<Route path="/detail/:id" element={<DetailPage />} />
+				</Routes>
+			</Router>
+		</FilterContextProvider>
+	);
 };
