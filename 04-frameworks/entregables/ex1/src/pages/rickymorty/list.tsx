a0e620d9a618b71a1414fb5464c71d9cdd95ab79
acastells <36441239+acastells@users.noreply.github.com>
2023-12-19 17:48:16 +0100

    style and split into subcomponents

--- 04-frameworks/entregables/ex1/src/pages/rickymorty/list.tsx
+++ 04-frameworks/entregables/ex1/src/pages/rickymorty/list.tsx
@@ -12,6 +12,7 @@ import {
 	FormControl,
 	FormControlLabel,
 	Radio,
+	Grid,
 } from "@mui/material";
 
 import { useNavigate } from "react-router-dom";
@@ -113,17 +114,14 @@ export const ListPage: React.FC = () => {
 		);
 	};
 
-	return (
-		<Container maxWidth="md">
-			<Stack direction="row" justifyContent="flex-end" sx={{ mt: 2 }}>
-				<Button onClick={handleLogout}>Logout</Button>
-			</Stack>
-
-			<Stack>
-				<h2>Rick and Morty characters</h2>
+	function FilterSection() {
+		return (
+			<>
 				<TextField
 					label="Character Name"
 					variant="outlined"
+					sx={{ pb: 1 }}
+					fullWidth
 					value={characterFilterOptions.name}
 					onChange={(e) =>
 						setCharacterFilterOptions({
@@ -135,6 +133,8 @@ export const ListPage: React.FC = () => {
 				<TextField
 					label="Species"
 					variant="outlined"
+					sx={{ pb: 1 }}
+					fullWidth
 					value={characterFilterOptions.species}
 					onChange={(e) =>
 						setCharacterFilterOptions({
@@ -146,6 +146,8 @@ export const ListPage: React.FC = () => {
 				<TextField
 					label="Type"
 					variant="outlined"
+					sx={{ pb: 2 }}
+					fullWidth
 					value={characterFilterOptions.type}
 					onChange={(e) =>
 						setCharacterFilterOptions({
@@ -155,72 +157,108 @@ export const ListPage: React.FC = () => {
 					}
 				/>
 
-				<FormControl>
-					<FormLabel>Status</FormLabel>
-					<RadioGroup
-						row
-						value={characterFilterOptions.status}
-						onChange={(e) =>
-							setCharacterFilterOptions({
-								...characterFilterOptions,
-								status: e.target.value,
-							})
-						}>
-						<FormControlLabel value="all" control={<Radio />} label="All" />
-						<FormControlLabel value="alive" control={<Radio />} label="Alive" />
-						<FormControlLabel value="dead" control={<Radio />} label="Dead" />
-						<FormControlLabel value="unknown" control={<Radio />} label="Unknown" />
-					</RadioGroup>
-				</FormControl>
+				<Grid container>
+					<Grid item xs={6}>
+						<FormLabel>Status</FormLabel>
+						<RadioGroup
+							value={characterFilterOptions.status}
+							onChange={(e) =>
+								setCharacterFilterOptions({
+									...characterFilterOptions,
+									status: e.target.value,
+								})
+							}>
+							<FormControlLabel value="all" control={<Radio />} label="All" />
+							<FormControlLabel value="alive" control={<Radio />} label="Alive" />
+							<FormControlLabel value="dead" control={<Radio />} label="Dead" />
+							<FormControlLabel value="unknown" control={<Radio />} label="Unknown" />
+						</RadioGroup>
+					</Grid>
 
-				<FormControl>
-					<FormLabel>Gender</FormLabel>
-					<RadioGroup
-						row
-						value={characterFilterOptions.gender}
-						onChange={(e) =>
-							setCharacterFilterOptions({
-								...characterFilterOptions,
-								gender: e.target.value,
-							})
-						}>
-						<FormControlLabel value="all" control={<Radio />} label="All" />
-						<FormControlLabel value="female" control={<Radio />} label="Female" />
-						<FormControlLabel value="male" control={<Radio />} label="Male" />
-						<FormControlLabel
-							value="genderless"
-							control={<Radio />}
-							label="Genderless"
-						/>
-						<FormControlLabel value="unknown" control={<Radio />} label="Unknown" />
-					</RadioGroup>
-				</FormControl>
+					<Grid item xs={6}>
+						<FormLabel>Gender</FormLabel>
+						<RadioGroup
+							value={characterFilterOptions.gender}
+							onChange={(e) =>
+								setCharacterFilterOptions({
+									...characterFilterOptions,
+									gender: e.target.value,
+								})
+							}>
+							<FormControlLabel value="all" control={<Radio />} label="All" />
+							<FormControlLabel value="female" control={<Radio />} label="Female" />
+							<FormControlLabel value="male" control={<Radio />} label="Male" />
+							<FormControlLabel
+								value="genderless"
+								control={<Radio />}
+								label="Genderless"
+							/>
+							<FormControlLabel value="unknown" control={<Radio />} label="Unknown" />
+						</RadioGroup>
+					</Grid>
+				</Grid>
+			</>
+		);
+	}
+
+	function TableSection() {
+		return (
+			<>
+				<PaginationInfoComponent />
+
+				<Box className="character-list-table" sx={{ mt: 2 }}>
+					<span className="list-header">Avatar</span>
+					<span className="list-header">Id</span>
+					<span className="list-header">Name</span>
+					<span className="list-header">Status</span>
+					<span className="list-header">Species</span>
+					<span className="list-header">Type</span>
+					<span className="list-header">Gender</span>
+					{characters.map((character) => (
+						<React.Fragment key={character.id}>
+							<img src={character.image} />
+							<span>{character.id}</span>
+							<Link to={`/rickandmorty/detail/${character.id}`}>
+								{character.name}
+							</Link>
+							<span>{character.status}</span>
+							<span>{character.species}</span>
+							<span>{character.type}</span>
+							<span>{character.gender}</span>
+						</React.Fragment>
+					))}
+				</Box>
+
+				<PaginationInfoComponent />
+			</>
+		);
+	}
+
+	return (
+		<Container maxWidth="lg">
+			<Stack direction="row" justifyContent="flex-end" sx={{ mt: 2 }}>
+				<Button onClick={handleLogout}>Logout</Button>
 			</Stack>
 
-			<PaginationInfoComponent />
+			<h2
+				style={{
+					textAlign: "center",
+					backgroundColor: "#0aafc8",
+					padding: 8,
+					borderRadius: 8,
+				}}>
+				Rick and Morty characters
+			</h2>
 
-			<Box className="character-list-table" sx={{ mt: 2 }}>
-				<span className="list-header">Avatar</span>
-				<span className="list-header">Id</span>
-				<span className="list-header">Name</span>
-				<span className="list-header">Status</span>
-				<span className="list-header">Species</span>
-				<span className="list-header">Type</span>
-				<span className="list-header">Gender</span>
-				{characters.map((character) => (
-					<React.Fragment key={character.id}>
-						<img src={character.image} />
-						<span>{character.id}</span>
-						<Link to={`/rickandmorty/detail/${character.id}`}>{character.name}</Link>
-						<span>{character.status}</span>
-						<span>{character.species}</span>
-						<span>{character.type}</span>
-						<span>{character.gender}</span>
-					</React.Fragment>
-				))}
-			</Box>
+			<Grid container spacing={8}>
+				<Grid item xs={4}>
+					<FilterSection />
+				</Grid>
 
-			<PaginationInfoComponent />
+				<Grid item xs={8}>
+					<TableSection />
+				</Grid>
+			</Grid>
 		</Container>
 	);
 };
