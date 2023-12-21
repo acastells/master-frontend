147f989001de1bb935cde9dfc21b6a68ec9f744e
acastells <36441239+acastells@users.noreply.github.com>
2023-12-21 20:36:01 +0100

    change login to adapt to pod architecture

---
+++ 04-frameworks/entregables/ex1/src/pods/login/login.container.tsx
@@ -0,0 +1,6 @@
+import React from "react";
+import { Login } from "./login.component"
+
+export const LoginContainer: React.FC = () => {
+	return (<Login />)
+}
--- 04-frameworks/entregables/ex1/src/scenes/login.scene.tsx
+++ 04-frameworks/entregables/ex1/src/scenes/login.scene.tsx
@@ -1,57 +1,6 @@
-import { Button, Container, Stack, TextField } from "@mui/material";
+import { LoginContainer } from "@/pods/login";
 import React from "react";
-import { useNavigate } from "react-router-dom";
-import { routes } from "@/router";
 
 export const LoginScene: React.FC = () => {
-	const navigate = useNavigate();
-	const [username, setUsername] = React.useState("");
-	const [password, setPassword] = React.useState("");
-
-	const handleNavigation = (path: string) => {
-		if (username === "admin" && password === "test") {
-			navigate(path);
-		} else {
-			alert("User / password not valid, psst... admin / test");
-		}
-	};
-
-	return (
-		<Container maxWidth="sm">
-			<h1>Login</h1>
-			<Stack direction="row" justifyContent={"center"} sx={{ mt: 2 }}>
-				<div>
-					<TextField
-						label="Username"
-						variant="outlined"
-						onChange={(e) => setUsername(e.target.value)}
-						value={username}
-						sx={{ mb: 3 }}
-						fullWidth
-					/>
-					<TextField
-						label="Password"
-						variant="outlined"
-						onChange={(e) => setPassword(e.target.value)}
-						value={password}
-						sx={{ mb: 3 }}
-						fullWidth
-						type="password"
-					/>
-				</div>
-			</Stack>
-
-			<Stack sx={{ mt: 2 }}>
-				<Button type="submit" variant="outlined" onClick={() => handleNavigation(routes.github.list)}>
-					Login to Github Users API
-				</Button>
-				<Button
-					type="submit"
-					variant="outlined"
-					onClick={() => handleNavigation(routes.rickandmorty.list)}>
-					Login to Rick and Morty API
-				</Button>
-			</Stack>
-		</Container>
-	);
+	return <LoginContainer></LoginContainer>;
 };
