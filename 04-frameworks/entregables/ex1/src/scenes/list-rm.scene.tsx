807e4ebc5b174a6a36920fc1e3eeb2e5aabdc5b5
acastells <36441239+acastells@users.noreply.github.com>
2024-01-10 21:04:12 +0100

    fix bug rerendering when keyin on filter textfield

--- 04-frameworks/entregables/ex1/src/scenes/list-rm.scene.tsx
+++ 04-frameworks/entregables/ex1/src/scenes/list-rm.scene.tsx
@@ -2,25 +2,139 @@ import React from "react";
 import { Link } from "react-router-dom";
 
 import {
-	Button,
 	Box,
-	TextField,
+	Button,
 	Container,
-	Stack,
-	FormLabel,
-	RadioGroup,
 	FormControlLabel,
-	Radio,
+	FormLabel,
 	Grid,
+	Radio,
+	RadioGroup,
+	Stack,
+	TextField,
 } from "@mui/material";
 
-import { useNavigate } from "react-router-dom";
 import { getCharacters } from "@/api";
 import { useDebounce } from "@/customHooks/useDebounce";
+import { useNavigate } from "react-router-dom";
 
-import { ArrowBackIosNewOutlined, ArrowForwardIosOutlined } from "@mui/icons-material";
 import { routes } from "@/router";
 import { CharacterEntity, CharacterFilterOptionsEntity } from "@/vm";
+import { ArrowBackIosNewOutlined, ArrowForwardIosOutlined } from "@mui/icons-material";
+
+const FilterSection = ({ characterFilterOptions, setCharacterFilterOptions }) => {
+	return (
+		<>
+			<TextField
+				label="Character Name"
+				variant="outlined"
+				sx={{ pb: 1 }}
+				fullWidth
+				value={characterFilterOptions.name}
+				onChange={(e) =>
+					setCharacterFilterOptions({
+						...characterFilterOptions,
+						name: e.target.value,
+					})
+				}
+			/>
+			<TextField
+				label="Species"
+				variant="outlined"
+				sx={{ pb: 1 }}
+				fullWidth
+				value={characterFilterOptions.species}
+				onChange={(e) =>
+					setCharacterFilterOptions({
+						...characterFilterOptions,
+						species: e.target.value,
+					})
+				}
+			/>
+			<TextField
+				label="Type"
+				variant="outlined"
+				sx={{ pb: 2 }}
+				fullWidth
+				value={characterFilterOptions.type}
+				onChange={(e) =>
+					setCharacterFilterOptions({
+						...characterFilterOptions,
+						type: e.target.value,
+					})
+				}
+			/>
+
+			<Grid container>
+				<Grid item xs={6}>
+					<FormLabel>Status</FormLabel>
+					<RadioGroup
+						value={characterFilterOptions.status}
+						onChange={(e) =>
+							setCharacterFilterOptions({
+								...characterFilterOptions,
+								status: e.target.value,
+							})
+						}>
+						<FormControlLabel value="all" control={<Radio />} label="All" />
+						<FormControlLabel value="alive" control={<Radio />} label="Alive" />
+						<FormControlLabel value="dead" control={<Radio />} label="Dead" />
+						<FormControlLabel value="unknown" control={<Radio />} label="Unknown" />
+					</RadioGroup>
+				</Grid>
+
+				<Grid item xs={6}>
+					<FormLabel>Gender</FormLabel>
+					<RadioGroup
+						value={characterFilterOptions.gender}
+						onChange={(e) =>
+							setCharacterFilterOptions({
+								...characterFilterOptions,
+								gender: e.target.value,
+							})
+						}>
+						<FormControlLabel value="all" control={<Radio />} label="All" />
+						<FormControlLabel value="female" control={<Radio />} label="Female" />
+						<FormControlLabel value="male" control={<Radio />} label="Male" />
+						<FormControlLabel
+							value="genderless"
+							control={<Radio />}
+							label="Genderless"
+						/>
+						<FormControlLabel value="unknown" control={<Radio />} label="Unknown" />
+					</RadioGroup>
+				</Grid>
+			</Grid>
+		</>
+	);
+};
+
+const TableSection = ({ characters }) => {
+	return (
+		<>
+			<Box className="character-list-table" sx={{ mt: 2 }}>
+				<span className="list-header">Avatar</span>
+				<span className="list-header">Id</span>
+				<span className="list-header">Name</span>
+				<span className="list-header">Status</span>
+				<span className="list-header">Species</span>
+				<span className="list-header">Type</span>
+				<span className="list-header">Gender</span>
+				{characters.map((character) => (
+					<React.Fragment key={character.id}>
+						<img src={character.image} />
+						<span>{character.id}</span>
+						<Link to={routes.rickandmorty.detail(character.id)}>{character.name}</Link>
+						<span>{character.status}</span>
+						<span>{character.species}</span>
+						<span>{character.type}</span>
+						<span>{character.gender}</span>
+					</React.Fragment>
+				))}
+			</Box>
+		</>
+	);
+};
 
 export const ListRMScene: React.FC = () => {
 	const navigate = useNavigate();
@@ -29,12 +143,15 @@ export const ListRMScene: React.FC = () => {
 	const [characterFilterOptions, setCharacterFilterOptions] =
 		React.useState<CharacterFilterOptionsEntity>({
 			page: 1,
+			name: "",
 			status: "all",
+			species: "",
+			type: "",
 			gender: "all",
 		});
 	const debouncedFilterOptions = useDebounce<CharacterFilterOptionsEntity>(
 		characterFilterOptions,
-		300
+		1000
 	);
 
 	const emptyPaginationInfo = {
@@ -102,126 +219,6 @@ export const ListRMScene: React.FC = () => {
 		);
 	};
 
-	function FilterSection() {
-		return (
-			<>
-				<TextField
-					label="Character Name"
-					variant="outlined"
-					sx={{ pb: 1 }}
-					fullWidth
-					value={characterFilterOptions.name}
-					onChange={(e) =>
-						setCharacterFilterOptions({
-							...characterFilterOptions,
-							name: e.target.value,
-						})
-					}
-				/>
-				<TextField
-					label="Species"
-					variant="outlined"
-					sx={{ pb: 1 }}
-					fullWidth
-					value={characterFilterOptions.species}
-					onChange={(e) =>
-						setCharacterFilterOptions({
-							...characterFilterOptions,
-							species: e.target.value,
-						})
-					}
-				/>
-				<TextField
-					label="Type"
-					variant="outlined"
-					sx={{ pb: 2 }}
-					fullWidth
-					value={characterFilterOptions.type}
-					onChange={(e) =>
-						setCharacterFilterOptions({
-							...characterFilterOptions,
-							type: e.target.value,
-						})
-					}
-				/>
-
-				<Grid container>
-					<Grid item xs={6}>
-						<FormLabel>Status</FormLabel>
-						<RadioGroup
-							value={characterFilterOptions.status}
-							onChange={(e) =>
-								setCharacterFilterOptions({
-									...characterFilterOptions,
-									status: e.target.value,
-								})
-							}>
-							<FormControlLabel value="all" control={<Radio />} label="All" />
-							<FormControlLabel value="alive" control={<Radio />} label="Alive" />
-							<FormControlLabel value="dead" control={<Radio />} label="Dead" />
-							<FormControlLabel value="unknown" control={<Radio />} label="Unknown" />
-						</RadioGroup>
-					</Grid>
-
-					<Grid item xs={6}>
-						<FormLabel>Gender</FormLabel>
-						<RadioGroup
-							value={characterFilterOptions.gender}
-							onChange={(e) =>
-								setCharacterFilterOptions({
-									...characterFilterOptions,
-									gender: e.target.value,
-								})
-							}>
-							<FormControlLabel value="all" control={<Radio />} label="All" />
-							<FormControlLabel value="female" control={<Radio />} label="Female" />
-							<FormControlLabel value="male" control={<Radio />} label="Male" />
-							<FormControlLabel
-								value="genderless"
-								control={<Radio />}
-								label="Genderless"
-							/>
-							<FormControlLabel value="unknown" control={<Radio />} label="Unknown" />
-						</RadioGroup>
-					</Grid>
-				</Grid>
-			</>
-		);
-	}
-
-	function TableSection() {
-		return (
-			<>
-				<PaginationInfoComponent />
-
-				<Box className="character-list-table" sx={{ mt: 2 }}>
-					<span className="list-header">Avatar</span>
-					<span className="list-header">Id</span>
-					<span className="list-header">Name</span>
-					<span className="list-header">Status</span>
-					<span className="list-header">Species</span>
-					<span className="list-header">Type</span>
-					<span className="list-header">Gender</span>
-					{characters.map((character) => (
-						<React.Fragment key={character.id}>
-							<img src={character.image} />
-							<span>{character.id}</span>
-							<Link to={routes.rickandmorty.detail(character.id)}>
-								{character.name}
-							</Link>
-							<span>{character.status}</span>
-							<span>{character.species}</span>
-							<span>{character.type}</span>
-							<span>{character.gender}</span>
-						</React.Fragment>
-					))}
-				</Box>
-
-				<PaginationInfoComponent />
-			</>
-		);
-	}
-
 	return (
 		<Container maxWidth="lg">
 			<Stack direction="row" justifyContent="flex-end" sx={{ mt: 2 }}>
@@ -240,11 +237,16 @@ export const ListRMScene: React.FC = () => {
 
 			<Grid container spacing={8}>
 				<Grid item xs={4}>
-					<FilterSection />
+					<FilterSection
+						characterFilterOptions={characterFilterOptions}
+						setCharacterFilterOptions={setCharacterFilterOptions}
+					/>
 				</Grid>
 
 				<Grid item xs={8}>
-					<TableSection />
+					<PaginationInfoComponent />
+					<TableSection characters={characters} />
+					<PaginationInfoComponent />
 				</Grid>
 			</Grid>
 		</Container>
