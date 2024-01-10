2dd6dfc06a4b9211d02011ad8d3c1c57e6d82e34
acastells <36441239+acastells@users.noreply.github.com>
2024-01-10 18:42:55 +0100

    using hash router

--- 04-frameworks/entregables/ex1/src/router/app.router.tsx
+++ 04-frameworks/entregables/ex1/src/router/app.router.tsx
@@ -1,11 +1,11 @@
-import React from "react";
-import { BrowserRouter, Route, Routes } from "react-router-dom";
-import { switchRoutes } from "./routes";
 import { DetailGHScene, DetailRMScene, ListGHScene, ListRMScene, LoginScene } from "@/scenes";
+import React from "react";
+import { HashRouter, Route, Routes } from "react-router-dom";
+import { switchRoutes } from "./routes";
 
 export const AppRouter = () => {
 	return (
-		<BrowserRouter>
+		<HashRouter>
 			<Routes>
 				<Route path={switchRoutes.login} element={<LoginScene />} />
 				<Route path={switchRoutes.github.list} element={<ListGHScene />} />
@@ -14,6 +14,6 @@ export const AppRouter = () => {
 				<Route path={switchRoutes.rickandmorty.detail} element={<DetailRMScene />} />				
 				<Route path="*" element={<div>404! not found!</div>} />
 			</Routes>
-		</BrowserRouter>
+		</HashRouter>
 	);
 };
