4641df80a93dcb9097a7ea77966aa7611cdd5e29
acastells <36441239+acastells@users.noreply.github.com>
2024-01-16 11:12:47 +0100

    adapt scene to work with pictureinfo model

--- 04-frameworks/entregables/ex2/src/scenes/alpacas.scene.tsx
+++ 04-frameworks/entregables/ex2/src/scenes/alpacas.scene.tsx
@@ -1,15 +1,27 @@
 import { Checkbox, FormControlLabel, ImageList, ImageListItem } from "@mui/material";
 import { LayoutWithCart } from "../layout/layoutWithCart.layout";
+import { PictureInfo } from "@/vm/vm";
 
 export const AlpacasScene: React.FC = () => {
+	const itemData: PictureInfo[] = [];
+	for (let i = 1; i <= 4; i++) {
+		const data = {
+			id: `assets/alpacas${i}`,
+			picUrl: `/src/assets/alpacas${i}.jpg`,
+			title: `Alpacas ${i}`,
+			selected: false
+		};
+		itemData.push(data);
+	}
+
 	return (
 		<LayoutWithCart>
 			<ImageList cols={4}>
 				{itemData.map((item) => (
-					<ImageListItem key={item.img} sx={{p:4}}>
+					<ImageListItem key={item.id} sx={{ p: 4 }}>
 						<img
-							srcSet={`${item.img}?w=164&h=164&fit=crop&auto=format&dpr=2 2x`}
-							src={`${item.img}?w=164&h=164&fit=crop&auto=format`}
+							srcSet={`${item.picUrl}?w=164&h=164&fit=crop&auto=format&dpr=2 2x`}
+							src={`${item.picUrl}?w=164&h=164&fit=crop&auto=format`}
 							alt={item.title}
 							loading="lazy"
 						/>
@@ -21,22 +33,3 @@ export const AlpacasScene: React.FC = () => {
 		</LayoutWithCart>
 	);
 };
-
-const itemData = [
-	{
-		img: "/src/assets/alpacas1.jpg",
-		title: "Alpacas 1",
-	},
-	{
-		img: "/src/assets/alpacas2.jpg",
-		title: "Alpacas 2",
-	},
-	{
-		img: "/src/assets/alpacas3.jpg",
-		title: "Alpacas 3",
-	},
-	{
-		img: "/src/assets/alpacas4.jpg",
-		title: "Alpacas 4",
-	},
-];
--- 04-frameworks/entregables/ex2/src/scenes/calves.scene.tsx
+++ 04-frameworks/entregables/ex2/src/scenes/calves.scene.tsx
@@ -1,5 +1,35 @@
+import { Checkbox, FormControlLabel, ImageList, ImageListItem } from "@mui/material";
 import { LayoutWithCart } from "../layout/layoutWithCart.layout";
+import { PictureInfo } from "@/vm/vm";
 
 export const CalvesScene: React.FC = () => {
-	return <LayoutWithCart></LayoutWithCart>;
+	const itemData: PictureInfo[] = [];
+	for (let i = 1; i <= 4; i++) {
+		const data = {
+			id: `assets/calves${i}`,
+			picUrl: `/src/assets/calves${i}.jpg`,
+			title: `Calves ${i}`,
+			selected: false
+		};
+		itemData.push(data);
+	}
+
+	return (
+		<LayoutWithCart>
+			<ImageList cols={4}>
+				{itemData.map((item) => (
+					<ImageListItem key={item.id} sx={{ p: 4 }}>
+						<img
+							srcSet={`${item.picUrl}?w=164&h=164&fit=crop&auto=format&dpr=2 2x`}
+							src={`${item.picUrl}?w=164&h=164&fit=crop&auto=format`}
+							alt={item.title}
+							loading="lazy"
+						/>
+						<p>{item.title}</p>
+						<FormControlLabel control={<Checkbox />} label="Buy" />
+					</ImageListItem>
+				))}
+			</ImageList>
+		</LayoutWithCart>
+	);
 };
