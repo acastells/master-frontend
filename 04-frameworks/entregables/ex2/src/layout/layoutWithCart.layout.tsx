8944a7f9bf0dc09bf437eee9084a2ff234be56ed
acastells <36441239+acastells@users.noreply.github.com>
2024-01-16 12:30:08 +0100

    adapt scenes to use the context Cart

--- 04-frameworks/entregables/ex2/src/layout/layoutWithCart.layout.tsx
+++ 04-frameworks/entregables/ex2/src/layout/layoutWithCart.layout.tsx
@@ -1,4 +1,6 @@
 // Layout.js
+import { PictureInfo } from "@/vm/vm";
+import DeleteIcon from "@mui/icons-material/Delete";
 import ShoppingCartIcon from "@mui/icons-material/ShoppingCart";
 import {
 	AppBar,
@@ -8,14 +10,18 @@ import {
 	Container,
 	CssBaseline,
 	Drawer,
+	Grid,
 	IconButton,
 	Toolbar,
+	Typography,
 } from "@mui/material";
 import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
+import { CartContext } from "../core/providers/cart/cartContext";
 
 export const LayoutWithCart = ({ children }: React.PropsWithChildren) => {
 	const navigate = useNavigate();
+	const { cart, removeImageFromCart } = React.useContext(CartContext);
 	const [isSidebarOpen, setSidebarOpen] = useState(false);
 
 	const toggleSidebar = () => {
@@ -25,7 +31,26 @@ export const LayoutWithCart = ({ children }: React.PropsWithChildren) => {
 	const SidebarContent = () => {
 		return (
 			<Box sx={{ width: 450, p: 2 }} component="div" role="presentation">
-				<h1>Cart</h1>
+				<Typography variant="h4" gutterBottom>
+					<ShoppingCartIcon /> Cart
+				</Typography>
+				{cart.map((image: PictureInfo) => (
+					<React.Fragment key={image.id}>
+						<Grid container alignItems="center" spacing={2}>
+							<Grid item>
+								<img src={image.picUrl} alt={image.title} width={150} />
+							</Grid>
+							<Grid item>
+								<Typography variant="subtitle1">{image.title}</Typography>
+							</Grid>
+							<Grid item>
+								<IconButton onClick={() => removeImageFromCart(image)}>
+									<DeleteIcon />
+								</IconButton>
+							</Grid>
+						</Grid>
+					</React.Fragment>
+				))}
 			</Box>
 		);
 	};
@@ -38,7 +63,7 @@ export const LayoutWithCart = ({ children }: React.PropsWithChildren) => {
 					<Button onClick={() => navigate("/")}>Image Marketplace</Button>
 					<div style={{ flexGrow: 1 }} />
 					<IconButton onClick={toggleSidebar}>
-						<Badge badgeContent={1} color="error">
+						<Badge badgeContent={cart.length} color="error">
 							<ShoppingCartIcon />
 						</Badge>
 					</IconButton>
--- 04-frameworks/entregables/ex2/src/scenes/alpacas.scene.tsx
+++ 04-frameworks/entregables/ex2/src/scenes/alpacas.scene.tsx
@@ -1,23 +1,34 @@
-import { Checkbox, FormControlLabel, ImageList, ImageListItem } from "@mui/material";
-import { LayoutWithCart } from "../layout/layoutWithCart.layout";
 import { PictureInfo } from "@/vm/vm";
+import { Checkbox, FormControlLabel, ImageList, ImageListItem } from "@mui/material";
+import React from "react";
+import { CartContext } from "../core/providers/cart/cartContext";
+import { LayoutWithCart } from "../layout/layoutWithCart.layout";
+import { getAlpacasInitialData } from "../vm/initialData";
 
 export const AlpacasScene: React.FC = () => {
-	const itemData: PictureInfo[] = [];
-	for (let i = 1; i <= 4; i++) {
-		const data = {
-			id: `assets/alpacas${i}`,
-			picUrl: `/src/assets/alpacas${i}.jpg`,
-			title: `Alpacas ${i}`,
-			selected: false
-		};
-		itemData.push(data);
-	}
+	const { cart, addImageToCart, removeImageFromCart, isInCart } = React.useContext(CartContext);
+	const [products, setProducts] = React.useState<PictureInfo[]>(getAlpacasInitialData());
+
+	React.useEffect(() => {
+		setProducts((prevProducts) =>
+			prevProducts.map((product) =>
+				isInCart(product) ? { ...product, selected: true } : { ...product, selected: false }
+			)
+		);
+	}, [cart, isInCart]);
+
+	const handleCheckboxOnChange = (item: PictureInfo, checked: boolean) => {
+		if (checked) {
+			addImageToCart(item);
+		} else {
+			removeImageFromCart(item);
+		}
+	};
 
 	return (
 		<LayoutWithCart>
 			<ImageList cols={4}>
-				{itemData.map((item) => (
+				{products.map((item: PictureInfo) => (
 					<ImageListItem key={item.id} sx={{ p: 4 }}>
 						<img
 							srcSet={`${item.picUrl}?w=164&h=164&fit=crop&auto=format&dpr=2 2x`}
@@ -26,7 +37,17 @@ export const AlpacasScene: React.FC = () => {
 							loading="lazy"
 						/>
 						<p>{item.title}</p>
-						<FormControlLabel control={<Checkbox />} label="Buy" />
+						<FormControlLabel
+							control={
+								<Checkbox
+									checked={item.selected}
+									onChange={(_e, checked) =>
+										handleCheckboxOnChange(item, checked)
+									}
+								/>
+							}
+							label="Buy"
+						/>
 					</ImageListItem>
 				))}
 			</ImageList>
