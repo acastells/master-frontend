9105b699a5470b6508c6056cf741dc058300f951
acastells <36441239+acastells@users.noreply.github.com>
2024-01-16 12:29:42 +0100

    create and use a new CartContext

---
+++ 04-frameworks/entregables/ex2/src/core/providers/cart/cartContext.tsx
@@ -0,0 +1,33 @@
+import { PictureInfo } from "@/vm/vm";
+import React, { PropsWithChildren, createContext } from "react";
+
+interface CartContextEntity {
+	cart: Array<PictureInfo>;
+	addImageToCart: (image: PictureInfo) => void;
+	removeImageFromCart: (image: PictureInfo) => void;
+	isInCart: (image: PictureInfo) => boolean;
+}
+
+export const CartContext = createContext<CartContextEntity>({} as CartContextEntity);
+
+export const CartContextProvider: React.FC<PropsWithChildren> = ({ children }) => {
+	const [cart, setCart] = React.useState<PictureInfo[]>([]);
+
+	const addImageToCart = (image: PictureInfo) => {
+		setCart([...cart, image]);
+	};
+
+	const removeImageFromCart = (image: PictureInfo) => {
+		setCart(cart.filter((item) => item.id !== image.id));
+	};
+
+	const isInCart = (image: PictureInfo): boolean => {
+		return cart.some((item) => item.id === image.id);
+	};
+
+	return (
+		<CartContext.Provider value={{ cart, addImageToCart, removeImageFromCart, isInCart }}>
+			{children}
+		</CartContext.Provider>
+	);
+};
--- 04-frameworks/entregables/ex2/src/router/app.router.tsx
+++ 04-frameworks/entregables/ex2/src/router/app.router.tsx
@@ -1,18 +1,21 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import { AlpacasScene, CalvesScene, StoreScene } from "../scenes";
 import { switchRoutes } from "./routes";
+import { CartContextProvider } from "../core/providers/cart/cartContext";
 
 export const AppRouter: React.FC = () => {
 	return (
-		<BrowserRouter>
-			<Routes>
-				<Route path={switchRoutes.home} element={<StoreScene />} />
-				<Route path={switchRoutes.store} element={<StoreScene />} />
-				<Route path={switchRoutes.alpacas} element={<AlpacasScene />} />
-				<Route path={switchRoutes.calves} element={<CalvesScene />} />
-				<Route path={switchRoutes.checkout} element={<h1>Not implemented</h1>} />
-				<Route path="*" element={<div>404! not found!</div>} />
-			</Routes>
-		</BrowserRouter>
+		<CartContextProvider>
+			<BrowserRouter>
+				<Routes>
+					<Route path={switchRoutes.home} element={<StoreScene />} />
+					<Route path={switchRoutes.store} element={<StoreScene />} />
+					<Route path={switchRoutes.alpacas} element={<AlpacasScene />} />
+					<Route path={switchRoutes.calves} element={<CalvesScene />} />
+					<Route path={switchRoutes.checkout} element={<h1>Not implemented</h1>} />
+					<Route path="*" element={<div>404! not found!</div>} />
+				</Routes>
+			</BrowserRouter>
+		</CartContextProvider>
 	);
 };
