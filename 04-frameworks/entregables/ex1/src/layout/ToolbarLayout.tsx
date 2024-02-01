88532841ffa21babee4b1161f33591414b87e0c0
acastells <36441239+acastells@users.noreply.github.com>
2024-02-01 09:29:18 +0100

    add change api button on toolbar layout

--- 04-frameworks/entregables/ex1/src/layout/ToolbarLayout.tsx
+++ 04-frameworks/entregables/ex1/src/layout/ToolbarLayout.tsx
@@ -1,19 +1,31 @@
 import { routes } from "@/core/router";
 import { Button, Container, Stack } from "@mui/material";
 import React from "react";
-import { useNavigate } from "react-router-dom";
+import { useLocation, useNavigate } from "react-router-dom";
 
 export const ToolbarLayout: React.FC<React.PropsWithChildren> = ({ children }) => {
 	const navigate = useNavigate();
+	const { pathname } = useLocation();
 
 	const handleLogout = () => {
 		navigate(routes.login);
 	};
 
+	const changeApiPage = () => {
+		if (pathname === routes.rickandmorty.list) {
+			navigate(routes.github.list);
+		} else if (pathname === routes.github.list) {
+			navigate(routes.rickandmorty.list);
+		}
+	};
+
 	return (
 		<Container maxWidth="lg" sx={{ pb: 4 }}>
-			<Stack direction="row" justifyContent="flex-end" sx={{ mt: 1 }}>
-				<Button onClick={handleLogout}>Logout</Button>
+			<Stack direction="row" justifyContent="flex-end" sx={{ mt: 2 }} spacing={4}>
+				<Button onClick={changeApiPage}>Change API</Button>
+				<Button color={"warning"} onClick={handleLogout}>
+					Logout
+				</Button>
 			</Stack>
 			{children}
 		</Container>
