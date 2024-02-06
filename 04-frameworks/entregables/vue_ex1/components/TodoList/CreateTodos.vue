e01c3ff191e32ad314b4d122203728979ca2b027
acastells <36441239+acastells@users.noreply.github.com>
2024-02-06 18:42:30 +0100

    use tailwind to make it responsible

--- 04-frameworks/entregables/vue_ex1/components/TodoList/CreateTodos.vue
+++ 04-frameworks/entregables/vue_ex1/components/TodoList/CreateTodos.vue
@@ -1,19 +1,22 @@
 <template>
-	<UContainer>
+	<div class="container flex flex-row gap-8">
 		<UInput
 			color="primary"
 			variant="outline"
 			placeholder="Create Todo"
-			v-model="newTodoText">
+			v-model="newTodoText"
+			class="flex-grow">
 		</UInput>
-		<UButton variant="solid" color="primary" @click="addTodo">+</UButton>
-	</UContainer>
+		<UButton variant="solid" color="primary" @click="addTodo" class="flex-shrink-0"
+			>Create Todo</UButton
+		>
+	</div>
 </template>
 
 <script setup lang="ts">
 const store = useTodos();
-
 const newTodoText = ref("");
+
 const addTodo = () => {
 	if (newTodoText.value.trim() !== "") {
 		store.setters.addTodo(newTodoText.value);
@@ -22,12 +25,4 @@ const addTodo = () => {
 };
 </script>
 
-<style scoped>
-input[type="text"] {
-	border-width: 1px;
-}
-
-button {
-	cursor: pointer;
-}
-</style>
+<style scoped></style>
