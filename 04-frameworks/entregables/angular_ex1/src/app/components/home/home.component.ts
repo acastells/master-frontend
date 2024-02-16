2254dc05308c7e0d949c48674f843d2cb50c8da7
acastells <36441239+acastells@users.noreply.github.com>
2024-02-16 14:27:18 +0100

    implement basic home component

--- 04-frameworks/entregables/angular_ex1/src/app/components/home/home.component.ts
+++ 04-frameworks/entregables/angular_ex1/src/app/components/home/home.component.ts
@@ -1,12 +1,17 @@
 import { Component } from '@angular/core';
+import { FooterComponent } from '../footer/footer.component';
+import { PublicMenuComponent } from '../public-menu/public-menu.component';
+import { PublicHeaderComponent } from '../public-header/public-header.component';
+import { PrivateMenuComponent } from '../private-menu/private-menu.component';
+import { PrivateHeaderComponent } from '../private-header/private-header.component';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [],
+  imports: [FooterComponent, PublicMenuComponent, PublicHeaderComponent, PrivateMenuComponent, PrivateHeaderComponent],
   templateUrl: './home.component.html',
-  styleUrl: './home.component.scss'
+  styleUrl: './home.component.scss',
 })
 export class HomeComponent {
-
+  loggedIn = false;
 }
