15a7c26532db048492678a8431de3dca562ec176
acastells <36441239+acastells@users.noreply.github.com>
2024-02-16 14:39:57 +0100

    implement basic public header

--- 04-frameworks/entregables/angular_ex1/src/app/components/public-header/public-header.component.ts
+++ 04-frameworks/entregables/angular_ex1/src/app/components/public-header/public-header.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { SharedModule } from '../../shared-module/shared-module.module';
 
 @Component({
   selector: 'app-public-header',
   standalone: true,
-  imports: [],
+  imports: [SharedModule],
   templateUrl: './public-header.component.html',
   styleUrl: './public-header.component.scss'
 })
