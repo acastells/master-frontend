cc5e1cfc3579b9cda7ee793f1d6504bb239d05bc
acastells <36441239+acastells@users.noreply.github.com>
2024-02-19 14:07:25 +0100

    make home for only not logged in

--- 04-frameworks/entregables/angular_ex1/src/app/components/home/home.component.ts
+++ 04-frameworks/entregables/angular_ex1/src/app/components/home/home.component.ts
@@ -1,32 +1,13 @@
 import { Component } from '@angular/core';
-import { FooterComponent } from '../footer/footer.component';
-import { PublicHeaderComponent } from '../public-header/public-header.component';
-import { PrivateHeaderComponent } from '../private-header/private-header.component';
 import { SharedModule } from '../../shared-module/shared-module.module';
-import { AuthService } from '../../services/auth.service';
 
 @Component({
   selector: 'app-home',
   standalone: true,
   imports: [
-    SharedModule,
-    FooterComponent,
-    PublicHeaderComponent,
-    PrivateHeaderComponent,
+    SharedModule
   ],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss',
 })
-export class HomeComponent {
-  isLoggedIn: boolean = false;
-
-  constructor(private authService: AuthService) {}
-
-  ngOnInit() {
-    this.authService
-      .getAuthChangedObservable()
-      .subscribe((isAuthenticated: boolean) => {
-        this.isLoggedIn = isAuthenticated;
-      });
-  }
-}
+export class HomeComponent {}
