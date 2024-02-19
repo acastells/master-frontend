afd0c2764851674d15f6167eecfdc9c3253da265
acastells <36441239+acastells@users.noreply.github.com>
2024-02-19 14:08:32 +0100

    add logo and logout method

--- 04-frameworks/entregables/angular_ex1/src/app/components/private-header/private-header.component.ts
+++ 04-frameworks/entregables/angular_ex1/src/app/components/private-header/private-header.component.ts
@@ -1,13 +1,20 @@
 import { Component } from '@angular/core';
 import { SharedModule } from '../../shared-module/shared-module.module';
+import { AuthService } from '../../services/auth.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-private-header',
   standalone: true,
   imports: [SharedModule],
   templateUrl: './private-header.component.html',
-  styleUrl: './private-header.component.scss'
+  styleUrl: './private-header.component.scss',
 })
 export class PrivateHeaderComponent {
+  constructor(private authService: AuthService, private router: Router) {}
 
+  onLogout() {
+    this.authService.logout();
+    this.router.navigate(['/']);
+  }
 }
