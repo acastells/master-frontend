ae4dce52aab352f7eacdfc649d49516e700436f2
acastells <36441239+acastells@users.noreply.github.com>
2024-02-19 14:19:58 +0100

    add missing routes and components

--- 04-frameworks/entregables/angular_ex1/src/app/app.routes.ts
+++ 04-frameworks/entregables/angular_ex1/src/app/app.routes.ts
@@ -3,6 +3,9 @@ import { HomeComponent } from './components/home/home.component';
 import { LoginComponent } from './components/login/login.component';
 import { AboutComponent } from './components/about/about.component';
 import { GalleryComponent } from './components/gallery/gallery.component';
+import { DashboardComponent } from './components/dashboard/dashboard.component';
+import { CrudComponent } from './components/crud/crud.component';
+import { ProfileComponent } from './components/profile/profile.component';
 
 export const routes: Routes = [
 	// public
@@ -12,8 +15,8 @@ export const routes: Routes = [
 	{ path: 'about', component: AboutComponent },
 
 	// private
-	{ path: 'dashboard', component: GalleryComponent },
+	{ path: 'dashboard', component: DashboardComponent },
 	{ path: 'gallery', component: GalleryComponent },
-	{ path: 'crud', component: GalleryComponent },
-	{ path: 'profile', component: GalleryComponent },
+	{ path: 'crud', component: CrudComponent },
+	{ path: 'profile', component: ProfileComponent },
   ];
