48c2b9940d646974f71f2c879839ff6f26d13048
acastells <36441239+acastells@users.noreply.github.com>
2024-02-19 11:23:57 +0100

    implement basic private header

--- 04-frameworks/entregables/angular_ex1/src/app/components/private-header/private-header.component.ts
+++ 04-frameworks/entregables/angular_ex1/src/app/components/private-header/private-header.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { SharedModule } from '../../shared-module/shared-module.module';
 
 @Component({
   selector: 'app-private-header',
   standalone: true,
-  imports: [],
+  imports: [SharedModule],
   templateUrl: './private-header.component.html',
   styleUrl: './private-header.component.scss'
 })
