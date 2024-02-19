066bc0db82e0719bb2e1f40dee6bc1fea462f50f
acastells <36441239+acastells@users.noreply.github.com>
2024-02-19 14:07:49 +0100

    use reactive forms for errors on login

--- 04-frameworks/entregables/angular_ex1/src/app/components/login/login.component.ts
+++ 04-frameworks/entregables/angular_ex1/src/app/components/login/login.component.ts
@@ -1,29 +1,61 @@
+import { NgIf } from '@angular/common';
 import { Component } from '@angular/core';
-import { SharedModule } from '../../shared-module/shared-module.module';
-import { FormsModule } from '@angular/forms';
-import { AuthService } from '../../services/auth.service';
+import {
+  FormControl,
+  FormGroup,
+  FormsModule,
+  ReactiveFormsModule,
+  ValidationErrors,
+  Validators,
+} from '@angular/forms';
 import { Router } from '@angular/router';
+import { AuthService } from '../../services/auth.service';
+import { SharedModule } from '../../shared-module/shared-module.module';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [SharedModule, FormsModule],
+  imports: [SharedModule, FormsModule, NgIf, ReactiveFormsModule],
   templateUrl: './login.component.html',
   styleUrl: './login.component.scss',
 })
 export class LoginComponent {
-  username: string = '';
-  password: string = '';
+  error: string = "";
+  form: FormGroup = new FormGroup({
+    username: new FormControl('', Validators.email),
+    password: new FormControl('', Validators.required),
+  });
 
-  constructor(private authService: AuthService, private router:Router){}
+  constructor(private authService: AuthService, private router: Router) {}
 
   onLogin() {
-    const loggedIn = this.authService.login(this.username, this.password)
-
-    if(loggedIn){
-      this.router.navigate(["/"])
-    } else {
-      alert("Wrong credentials")
+    this.error = "";
+    if (!this.form.valid) {
+      this.getFormValidationErrors();
+      return;
     }
+
+    const formValue = this.form.value;
+    const loggedIn = this.authService.login(
+      formValue.username,
+      formValue.password
+    );    
+
+    if (loggedIn) {
+      this.router.navigate(['/']);
+    } else {
+      this.error = "Wrong credentials"
+    }
+  }
+
+  getFormValidationErrors() {
+    Object.keys(this.form.controls).forEach((key) => {
+      const controlErrors = this.form.get(key)?.errors;
+      if (controlErrors) {
+        Object.keys(controlErrors).forEach((keyError) => {
+          this.error += `${key} has a '${keyError}' error \n`;
+        });
+      }
+    });
   }
 }
