56e06c6bbcb16d7bf4fa640ab89720ba2bb29020
acastells <36441239+acastells@users.noreply.github.com>
2024-02-19 11:12:39 +0100

    implement basic auth service and use it in login component

--- 04-frameworks/entregables/angular_ex1/src/app/components/login/login.component.ts
+++ 04-frameworks/entregables/angular_ex1/src/app/components/login/login.component.ts
@@ -1,12 +1,29 @@
 import { Component } from '@angular/core';
+import { SharedModule } from '../../shared-module/shared-module.module';
+import { FormsModule } from '@angular/forms';
+import { AuthService } from '../../services/auth.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [],
+  imports: [SharedModule, FormsModule],
   templateUrl: './login.component.html',
-  styleUrl: './login.component.scss'
+  styleUrl: './login.component.scss',
 })
 export class LoginComponent {
+  username: string = '';
+  password: string = '';
 
+  constructor(private authService: AuthService, private router:Router){}
+
+  onLogin() {
+    const loggedIn = this.authService.login(this.username, this.password)
+
+    if(loggedIn){
+      this.router.navigate(["/"])
+    } else {
+      alert("Wrong credentials")
+    }
+  }
 }
--- 04-frameworks/entregables/angular_ex1/src/app/services/auth.service.ts
+++ 04-frameworks/entregables/angular_ex1/src/app/services/auth.service.ts
@@ -1,28 +1,37 @@
 import { Injectable } from '@angular/core';
+import { BehaviorSubject } from 'rxjs';
 
 @Injectable({
   providedIn: 'root',
 })
 export class AuthService {
   private isLoggedIn: boolean = false;
+  private authChangedSubject: BehaviorSubject<boolean> = new BehaviorSubject<boolean>(this.isLoggedIn);
 
   constructor() {}
 
   login(username: string, password: string): boolean {
     if (username === 'admin' && password === 'test') {
       this.isLoggedIn = true;
+      this.authChangedSubject.next(this.isLoggedIn);
       return true;
     } else {
       this.isLoggedIn = false;
+      this.authChangedSubject.next(this.isLoggedIn);
       return false;
     }
   }
 
   logout(): void {
     this.isLoggedIn = false;
+    this.authChangedSubject.next(this.isLoggedIn);
   }
 
   isAuthenticated(): boolean {
     return this.isLoggedIn;
+  }
+
+  getAuthChangedObservable(): BehaviorSubject<boolean> {
+    return this.authChangedSubject;
   }
 }
