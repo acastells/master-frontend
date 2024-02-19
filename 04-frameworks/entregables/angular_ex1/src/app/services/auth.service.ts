0073de6567a03666dfd0f91d25a6ecf0b0cee5a3
acastells <36441239+acastells@users.noreply.github.com>
2024-02-19 10:46:38 +0100

    add auth service

---
+++ 04-frameworks/entregables/angular_ex1/src/app/services/auth.service.ts
@@ -0,0 +1,28 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class AuthService {
+  private isLoggedIn: boolean = false;
+
+  constructor() {}
+
+  login(username: string, password: string): boolean {
+    if (username === 'admin' && password === 'test') {
+      this.isLoggedIn = true;
+      return true;
+    } else {
+      this.isLoggedIn = false;
+      return false;
+    }
+  }
+
+  logout(): void {
+    this.isLoggedIn = false;
+  }
+
+  isAuthenticated(): boolean {
+    return this.isLoggedIn;
+  }
+}
