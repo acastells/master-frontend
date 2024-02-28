15b344bbb8b4464658554b28ea723fc652927b93
acastells <36441239+acastells@users.noreply.github.com>
2024-02-28 18:27:50 +0100

    implement project mapper  test

---
+++ 05-testing/laboratorio_1/src/pods/project/project.mapper.spec.ts
@@ -0,0 +1,82 @@
+import * as apiModel from './api/project.api-model';
+import * as viewModel from './project.vm';
+import { mapProjectFromApiToVm } from './project.mapper';
+
+describe('pods/project/project.mapper.ts mapProjectFromApiToVm', () => {
+  it('map project from API to view model with valid project + no employees, result project', () => {
+    // Arrange
+    const project: apiModel.Project = {
+      id: '1',
+      name: 'react app',
+      isActive: true,
+      employees: [],
+    };
+
+    // Act
+    const result: viewModel.Project = mapProjectFromApiToVm(project);
+
+    // Assert
+    const expectedResult: viewModel.Project = {
+      id: '1',
+      name: 'react app',
+      isActive: true,
+      employees: [],
+    };
+    expect(result).toEqual(expectedResult);
+  });
+
+  it('map project from API to view model with null/undefined project, result new project', () => {
+    // Arrange
+    const projectNull = null;
+    const projectUndefined = undefined;
+
+    // Act
+    const resultNull = mapProjectFromApiToVm(projectNull);
+    const resultUndefined = mapProjectFromApiToVm(projectUndefined);
+
+    // Assert
+    const emptyProject: viewModel.Project = {
+      id: '',
+      name: '',
+      externalId: '',
+      comments: '',
+      isActive: false,
+      employees: [],
+    };
+
+    expect(resultNull).toEqual(emptyProject);
+    expect(resultUndefined).toEqual(emptyProject);
+  });
+
+  it('map project from API to view model with valid project + multiple employees, result project', () => {
+    // Arrange
+    const project: apiModel.Project = {
+      id: '1',
+      name: 'react app',
+      isActive: true,
+      externalId: 'externalID_123',
+      comments: 'Comment test',
+      employees: [
+        { id: '1', isAssigned: false, employeeName: 'Victor' },
+        { id: '2', isAssigned: true, employeeName: 'Jaime' },
+      ],
+    };
+
+    // Act
+    const result: viewModel.Project = mapProjectFromApiToVm(project);
+
+    // Assert
+    const expectedResult: viewModel.Project = {
+      id: '1',
+      name: 'react app',
+      isActive: true,
+      externalId: 'externalID_123',
+      comments: 'Comment test',
+      employees: [
+        { id: '1', isAssigned: false, employeeName: 'Victor' },
+        { id: '2', isAssigned: true, employeeName: 'Jaime' },
+      ],
+    };
+    expect(result).toEqual(expectedResult);
+  });
+});
