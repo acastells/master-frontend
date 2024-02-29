aedf5dfda8a2be0c94dab59164e5fccdd77c8d14
acastells <36441239+acastells@users.noreply.github.com>
2024-02-29 20:41:16 +0100

    init cypress e2e config

---
+++ 05-testing/laboratorio_1/cypress/e2e/login.spec.ts
@@ -0,0 +1,47 @@
+import * as LoginContainer from "../../src/pods/login/login.api"
+
+describe('Login specs', () => {
+  it('visit the login page', () => {
+    cy.visit('/');
+  });
+
+  it('should name input has the focus when it clicks on it', () => {
+    // Act
+    cy.visit('/');
+    cy.findByRole('textbox', {name:"Usuario *"}).click();
+
+    // Assert
+    cy.findByRole('textbox', {name:"Usuario *"}).should('have.focus');
+  });
+
+  it('should show an alert with a message when type invalid credentials', () => {
+    // Arrange
+    const user = 'admin';
+    const password = '1234';
+
+    // Act
+    cy.visit('/');
+    cy.findByRole('Usuario *').as("userInput")
+    cy.findByLabelText('Contraseña *').as("passwordInput")
+	cy.spy(LoginContainer, "isValidLogin").as("loginSpy")
+
+    cy.get('@userInput').type(user);
+    cy.get('@passwordInput').type(password);
+    cy.findByRole('button', { name: 'Login' }).click();
+
+    // Assert
+    cy.get('@userInput').should('have.value', user);
+    cy.get('@passwordInput').should('have.value', password);
+    cy.get('@loginSpy').should(
+      'have.been.called'
+    );
+  });
+
+  it('should navigate when type valid credentials', () => {
+    // Arrange
+
+    // Act
+
+    // Assert
+  });
+});
