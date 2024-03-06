f40dcaad9e6fb0e93c6181c18a9dfcfe35b311a8
acastells <36441239+acastells@users.noreply.github.com>
2024-03-06 17:33:39 +0100

    configure CI github for e2e tests

--- 05-testing/laboratorio_1/cypress/e2e/login.spec.ts
+++ 05-testing/laboratorio_1/cypress/e2e/login.spec.ts
@@ -1,4 +1,4 @@
-import * as LoginContainer from "../../src/pods/login/login.api"
+import * as LoginContainer from '../../src/pods/login/login.api';
 
 describe('Login specs', () => {
   it('visit the login page', () => {
@@ -8,10 +8,10 @@ describe('Login specs', () => {
   it('should name input has the focus when it clicks on it', () => {
     // Act
     cy.visit('/');
-    cy.findByRole('textbox', {name:"Usuario *"}).click();
+    cy.findByRole('textbox', { name: 'Usuario *' }).click();
 
     // Assert
-    cy.findByRole('textbox', {name:"Usuario *"}).should('have.focus');
+    cy.findByRole('textbox', { name: 'Usuario *' }).should('have.focus');
   });
 
   it('should show an alert with a message when type invalid credentials', () => {
@@ -21,9 +21,9 @@ describe('Login specs', () => {
 
     // Act
     cy.visit('/');
-    cy.findByRole('Usuario *').as("userInput")
-    cy.findByLabelText('Contraseña *').as("passwordInput")
-	cy.spy(LoginContainer, "isValidLogin").as("loginSpy")
+    cy.findByRole('Usuario *').as('userInput');
+    cy.findByLabelText('Contraseña *').as('passwordInput');
+    cy.spy(LoginContainer, 'isValidLogin').as('loginSpy');
 
     cy.get('@userInput').type(user);
     cy.get('@passwordInput').type(password);
@@ -32,16 +32,27 @@ describe('Login specs', () => {
     // Assert
     cy.get('@userInput').should('have.value', user);
     cy.get('@passwordInput').should('have.value', password);
-    cy.get('@loginSpy').should(
-      'have.been.called'
-    );
+    cy.get('@loginSpy').should('have.been.called');
   });
 
   it('should navigate when type valid credentials', () => {
-    // Arrange
+    /*
+    +   // Arrange
+    +   const user = 'admin';
+    +   const password = 'test';
 
-    // Act
+    +   // Act
+    +   cy.visit('/');
+    +   cy.findByLabelText('Name').as('userInput');
+    +   cy.findByLabelText('Password').as('passwordInput');
 
-    // Assert
+    +   cy.get('@userInput').type(user);
+    +   cy.get('@passwordInput').type(password);
+    +   cy.findByRole('button', { name: 'Login' }).click();
+
+    +   // Assert
+    +   cy.url().should('equal', 'http://localhost:8080/#/hotel-collection');
+    */
   });
+  
 });
