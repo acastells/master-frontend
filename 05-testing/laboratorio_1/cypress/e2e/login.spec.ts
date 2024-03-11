59a2cb79e1328ce13480c1281871e0a045ce1cb9
acastells <36441239+acastells@users.noreply.github.com>
2024-03-11 08:48:56 +0100

    add tests for correct and incorrect credentials for login

--- 05-testing/laboratorio_1/cypress/e2e/login.spec.ts
+++ 05-testing/laboratorio_1/cypress/e2e/login.spec.ts
@@ -1,5 +1,3 @@
-import * as LoginContainer from '../../src/pods/login/login.api';
-
 describe('Login specs', () => {
   it('should name input has the focus when it clicks on it', () => {
     // Act
@@ -10,11 +8,33 @@ describe('Login specs', () => {
     cy.findByRole('textbox', { name: 'Usuario *' }).should('have.focus');
   });
 
-  it.only('should show an alert with a message when type invalid credentials', () => {
+  it('should show an alert with a message when type invalid credentials', () => {
     // Arrange
     const user = 'admin';
     const password = '1234';
-    cy.spy(LoginContainer, 'isValidLogin').as('isValidLoginSpy');
+
+    // Act
+    cy.visit('/');
+    cy.findByLabelText('Usuario *').as('userInput');
+    cy.findByLabelText('Contraseña *').as('passwordInput');
+
+    // Act type and click
+    cy.get('@userInput').type(user);
+    cy.get('@passwordInput').type(password);
+    cy.findByRole('button', { name: 'Login' }).click();
+
+    // Assert
+    cy.get('@userInput').should('have.value', user);
+    cy.get('@passwordInput').should('have.value', password);
+    cy.findByRole('alert', {
+      name: /Usuario y\/o password no válidos/i,
+    }).should('exist');
+  });
+
+  it('should navigate when type valid credentials', () => {
+    // Arrange
+    const user = 'admin';
+    const password = 'test';
 
     // Act
     cy.visit('/');
@@ -28,29 +48,6 @@ describe('Login specs', () => {
     // Assert
     cy.get('@userInput').should('have.value', user);
     cy.get('@passwordInput').should('have.value', password);
-    cy.findByRole('alert', {
-      name: /Usuario y\/o password no válidos/i,
-      timeout: 5000,
-    }).should('exist');
-    cy.get('@isValidLoginSpy').should('be.called');
-  });
-
-  it('should navigate when type valid credentials', () => {
-    // Arrange
-    const user = 'admin';
-    const password = 'test';
-    /*
-    +   // Act
-    +   cy.visit('/');
-    +   cy.findByLabelText('Name').as('userInput');
-    +   cy.findByLabelText('Password').as('passwordInput');
-
-    +   cy.get('@userInput').type(user);
-    +   cy.get('@passwordInput').type(password);
-    +   cy.findByRole('button', { name: 'Login' }).click();
-
-    +   // Assert
-    +   cy.url().should('equal', 'http://localhost:8080/#/project-list');
-    */
+    cy.url().should('equal', 'http://localhost:8080/#/submodule-list');
   });
 });
