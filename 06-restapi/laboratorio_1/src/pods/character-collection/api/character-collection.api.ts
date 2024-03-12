1bf38a5050e1e5cadc94c428bddd4d62a9874aba
acastells <36441239+acastells@users.noreply.github.com>
2024-03-12 12:57:12 +0100

    implement api code to adapt to multiple endpoints

--- 06-restapi/laboratorio_1/src/pods/character-collection/api/character-collection.api.ts
+++ 06-restapi/laboratorio_1/src/pods/character-collection/api/character-collection.api.ts
@@ -1,12 +1,20 @@
 import axios from 'axios';
 import { CharacterEntityApi } from './character-collection.api-model';
 
-
 export const getCharacterCollection = async (): Promise<CharacterEntityApi[]> => {
-  const url = `${process.env.API_ENDPOINT}/character`
+  let url: string;
+  
+  switch (process.env.API_ENDPOINT) {
+    case 'public_api':
+      url = `${process.env.PUBLIC_API_ENDPOINT}/character`;
+      break;
+    case 'json_server':
+      url = `${process.env.JSON_SERVER_ENDPOINT}/character`;
+      break;
+    default:
+      throw new Error('API_ENDPOINT unknown');
+  }
+
   const response = await axios.get(url);
-
-  const characterCollection: CharacterEntityApi[] = response.data.results;
-
-  return characterCollection;
+  return process.env.API_ENDPOINT === 'public_api' ? response.data.results : response.data;
 };
--- 06-restapi/laboratorio_1/src/pods/character/api/character.api.ts
+++ 06-restapi/laboratorio_1/src/pods/character/api/character.api.ts
@@ -2,7 +2,19 @@ import axios from 'axios';
 import { Character } from './character.api-model';
 
 export const getCharacter = async (id: number): Promise<Character> => {
-  const url = `${process.env.API_ENDPOINT}/character`;
+  let url: string;
+
+  switch (process.env.API_ENDPOINT) {
+    case 'public_api':
+      url = `${process.env.PUBLIC_API_ENDPOINT}/character`;
+      break;
+    case 'json_server':
+      url = `${process.env.JSON_SERVER_ENDPOINT}/character`;
+      break;
+    default:
+      throw new Error('API_ENDPOINT unknown');
+  }
+
   const response = await axios.get(`${url}/${id}`);
 
   const character: Character = response.data;
