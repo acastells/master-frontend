55b7a58960c6b01cf26fb296dab44155d648b4a9
acastells <36441239+acastells@users.noreply.github.com>
2024-03-13 13:58:46 +0100

    implement pagination on scroll to bottom in public_api

--- 06-restapi/laboratorio_1/src/pods/character-collection/api/character-collection.api.ts
+++ 06-restapi/laboratorio_1/src/pods/character-collection/api/character-collection.api.ts
@@ -1,40 +1,37 @@
 import axios from 'axios';
-import * as graphQLModel from 'pods/character/api/character.graphql-model';
 import { GraphQLClient, gql } from 'graphql-request';
 import * as apiModel from 'pods/character/api/character.api-model';
+import * as graphQLModel from 'pods/character/api/character.graphql-model';
 
-export const getCharacterCollection = async (): Promise<
-  apiModel.Character[]
-> => {
-  let url: string;
+export const getCharacterCollection = async (
+  page: number
+): Promise<apiModel.Character[]> => {
+  const endpoint =
+    process.env.API_ENDPOINT === 'public_api'
+      ? process.env.PUBLIC_API_ENDPOINT
+      : process.env.JSON_SERVER_ENDPOINT;
+  const url = `${endpoint}/character?page=${page}`;
 
-  switch (process.env.API_ENDPOINT) {
-    case 'public_api':
-      url = `${process.env.PUBLIC_API_ENDPOINT}/character`;
-      break;
-    case 'json_server':
-      url = `${process.env.JSON_SERVER_ENDPOINT}/character`;
-      break;
-    default:
-      throw new Error('API_ENDPOINT unknown');
+  try {
+    const response = await axios.get(url);
+    return process.env.API_ENDPOINT === 'public_api'
+      ? response.data.results
+      : response.data;
+  } catch (error) {
+    console.log(error);
+    return [];
   }
-
-  const response = await axios.get(url);
-  return process.env.API_ENDPOINT === 'public_api'
-    ? response.data.results
-    : response.data;
 };
 
-export const getCharacterCollectionGraphQL = async (): Promise<
-  graphQLModel.Character[]
-> => {
-  const url = 'https://rickandmortyapi.com/graphql';
-
+export const getCharacterCollectionGraphQL = async (
+  page: number
+): Promise<graphQLModel.Character[]> => {
+  const url = process.env.PUBLIC_GRAPHQL_ENPOINT;
   const graphQLClient = new GraphQLClient(url);
 
   const query = gql`
-    query {
-      characters(page: 0) {
+    query GetCharacters($page: Int) {
+      characters(page: $page) {
         results {
           id
           name
@@ -59,14 +56,15 @@ export const getCharacterCollectionGraphQL = async (): Promise<
     }
   `;
 
-  interface CharactersGraphQLResponse {
-    characters: {
-      results: graphQLModel.Character[];
-    };
+  try {
+    const response = await graphQLClient.request<{
+      characters: {
+        results: graphQLModel.Character[];
+      };
+    }>(query, { page });
+    return response.characters.results;
+  } catch (e) {
+    console.log(e);
+    return [];
   }
-
-  const response = await graphQLClient.request<CharactersGraphQLResponse>(
-    query
-  );
-  return response.characters.results
 };
--- 06-restapi/laboratorio_1/src/pods/character-collection/character-collection.hook.ts
+++ 06-restapi/laboratorio_1/src/pods/character-collection/character-collection.hook.ts
@@ -8,25 +8,57 @@ import * as React from 'react';
 import { getCharacterCollection, getCharacterCollectionGraphQL } from './api';
 
 export const useCharacterCollection = () => {
+  const [page, setPage] = React.useState(1);
   const [characterCollection, setCharacterCollection] = React.useState<
     Character[]
   >([]);
+  const [loading, setLoading] = React.useState(false);
 
-  const loadCharacterCollection = () => {
+  React.useEffect(() => {
+    if (process.env.API_ENDPOINT === "public_api"){
+      window.addEventListener('scroll', handleScrollDebounced);
+      return () => window.removeEventListener('scroll', handleScrollDebounced);
+    }
+  }, [loading]);
+
+  function debounce(func, wait = 500) {
+    let timeout;
+    return (...args) => {
+      clearTimeout(timeout);
+      timeout = setTimeout(() => func.apply(this, args), wait);
+    };
+  }
+
+  const handleScrollDebounced = debounce((e) => {
+    const windowHeight = window.innerHeight;
+    const documentHeight = document.body.offsetHeight;
+    const scrollTop = window.scrollY;
+    const isAtBottom = windowHeight + scrollTop >= documentHeight;
+
+    if (isAtBottom && loading === false) {
+      setPage(page + 1);
+      loadCharacterCollection(page + 1);
+    }
+  });
+
+  const loadCharacterCollection = (page = 1) => {
+    setLoading(true);
     const APIOption = localStorage.getItem('APIOption');
 
     if (APIOption === 'REST') {
-      getCharacterCollection().then((result) =>
-        setCharacterCollection(mapToCollection(result, mapCharacterFromApiToVm))
-      );
+      getCharacterCollection(page).then((result) => {
+        const collection = mapToCollection(result, mapCharacterFromApiToVm);
+        setCharacterCollection([...characterCollection, ...collection]);
+        setLoading(false);
+      });
     } else if (APIOption === 'GraphQL') {
-      getCharacterCollectionGraphQL().then((result) =>
-        setCharacterCollection(
-          mapToCollection(result, mapCharacterFromGraphQLToVm)
-        )
-      );
+      getCharacterCollectionGraphQL(page).then((result) => {
+        const collection = mapToCollection(result, mapCharacterFromGraphQLToVm);
+        setCharacterCollection([...characterCollection, ...collection]);
+        setLoading(false);
+      });
     } else {
-      throw(`APIOption: ${APIOption} not implemented`)
+      throw `APIOption: ${APIOption} not implemented`;
     }
   };
 
