f15db1523e0555416d9fd6781f72d01d66384940
acastells <36441239+acastells@users.noreply.github.com>
2024-03-13 11:47:29 +0100

    add mapper from GraphQL to VM

--- 06-restapi/laboratorio_1/src/pods/character/character.mappers.ts
+++ 06-restapi/laboratorio_1/src/pods/character/character.mappers.ts
@@ -1,5 +1,6 @@
 import * as apiModel from './api/character.api-model';
 import * as viewModel from './character.vm';
+import * as graphQLModel from './api/character.graphql-model';
 
 export const mapCharacterFromApiToVm = (
   character: apiModel.Character
@@ -14,6 +15,26 @@ export const mapCharacterFromApiToVm = (
   origin: character.origin,
   location: character.location,
   image: character.image,
-  episode: character.episode,
-  bestSentences: character.bestSentences || []
+  bestSentences: character.bestSentences || [],
+});
+
+export const mapCharacterFromGraphQLToVm = (
+  character: graphQLModel.Character
+): viewModel.Character => ({
+  ...character,
+  id: character.id,
+  name: character.name,
+  status: character.status,
+  species: character.species,
+  type: character.type,
+  gender: character.gender,
+  origin: {
+    name: character.origin.name,
+    url: character.origin.id,
+  },
+  location: {
+    name: `${character.location.name} - ${character.location.type} - ${character.location.dimension}`,
+    url: character.location.id,
+  },
+  bestSentences: [],
 });
--- 06-restapi/laboratorio_1/src/pods/character/character.vm.ts
+++ 06-restapi/laboratorio_1/src/pods/character/character.vm.ts
@@ -14,6 +14,5 @@ export interface Character {
     url: string;
   };
   image: string;
-  episode: string[];
   bestSentences: string[];
 }
